9b4ddc06c9c89fa81c24b09703440412da019c80
byashwan <b.yashwanth.reddy@intel.com>
2020-08-31 14:32:01 +0530

    Adaptive cards

--- componentDialog/cancelReservationDialog.js
+++ componentDialog/cancelReservationDialog.js
@@ -1,22 +1,29 @@
 const {WaterfallDialog, ComponentDialog } = require('botbuilder-dialogs');
 
-const {ConfirmPrompt, ChoicePrompt, DateTimePrompt, NumberPrompt, TextPrompt  } = require('botbuilder-dialogs');
+const {ConfirmPrompt, ChoicePrompt, DateTimePrompt, NumberPrompt, TextPrompt, ActivityPrompt } = require('botbuilder-dialogs');
 
 const {DialogSet, DialogTurnStatus } = require('botbuilder-dialogs');
 
 const {CardFactory} = require('botbuilder');
+var ACData = require("adaptivecards-templating");
 
-const RestaurantCard = require('../resources/adaptiveCards/Restaurantcard')
+const RestaurantCard = require('../resources/adaptiveCards/sample.json')
+
 const CARDS = [
     RestaurantCard
 ];
 
+var template = new ACData.Template(CARDS[0]);
+var cardPayload = template.expand({
+    $root: CARDS[0]
+ });
 const CHOICE_PROMPT    = 'CHOICE_PROMPT';
 const CONFIRM_PROMPT   = 'CONFIRM_PROMPT';
 const TEXT_PROMPT      = 'TEXT_PROMPT';
 const NUMBER_PROMPT    = 'NUMBER_PROMPT';
 const DATETIME_PROMPT  = 'DATETIME_PROMPT';
 const WATERFALL_DIALOG = 'WATERFALL_DIALOG';
+const CARD_PROMPT = 'CARD_PROMPT';
 var endDialog ='';
 
 class CancelReservationDialog extends ComponentDialog {
@@ -28,6 +35,7 @@ class CancelReservationDialog extends ComponentDialog {
 
 this.addDialog(new TextPrompt(TEXT_PROMPT));
 this.addDialog(new ChoicePrompt(CHOICE_PROMPT));
+this.addDialog(new ActivityPrompt(CARD_PROMPT,  this.inputValidator));
 this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT));
 this.addDialog(new NumberPrompt(NUMBER_PROMPT,this.noOfParticipantsValidator));
 this.addDialog(new DateTimePrompt(DATETIME_PROMPT));
@@ -66,21 +74,22 @@ await step.context.sendActivity({
     attachments: [CardFactory.adaptiveCard(CARDS[0])]
 });
 
-return await step.prompt(TEXT_PROMPT, '');
 
-      
+return await step.prompt(CARD_PROMPT);
+
 }
 
 
 async confirmStep(step){
-    step.values.reservationNo = step.result
-
-    var msg = ` You have entered following values: \n Reservation Number: ${step.values.reservationNo}`
-
+   // step.values.reservationNo = step.result
+   var msg = ` Did you mean : ${step.result.value.myColor}`;
+   return await step.prompt(CONFIRM_PROMPT, msg, ['yes', 'no']);
+  // return await step.prompt(TEXT_PROMPT, 'In what name reservation is to be made?: ${step.result.value.myColor}');
+    /*
+   var msg = ` You have entered following values: \n Reservation Number: ${step.result.value.myColor}`;
     await step.context.sendActivity(msg);
-
     return await step.prompt(CONFIRM_PROMPT, 'Are you sure that all values are correct and you want to CANCEL the reservation?', ['yes', 'no']);
-   
+   */
 }
 
 async summaryStep(step){
@@ -102,7 +111,22 @@ async summaryStep(step){
 async isDialogComplete(){
     return endDialog;
 }
+
+// If you remove this validation logic, it will cause an error cause this is mandatory for ActivityPrompt
+async inputValidator(promptContext){
+    const userInputObject = promptContext.recognized.value.value;
+
+    // You can add some validation logic for email address and phone number
+    // userInputObject.myEmail, userInputObject.myTel
+
+    return true;
 }
+
+}
+
+
+
+
 
 module.exports.CancelReservationDialog = CancelReservationDialog;
 
